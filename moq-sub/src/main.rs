3789326aef5d62782d1167096ee4d08619815048
Erik Åberg <erik.w.aberg@svt.se>
2024-01-17 10:19:15 +0100

    broken code

--- moq-sub/src/main.rs
+++ moq-sub/src/main.rs
@@ -45,13 +45,12 @@ async fn file_renamer(target: &PathBuf, filter_kind: &str) -> anyhow::Result<()>
 	inotify.watches().add(src_dir, WatchMask::CLOSE_WRITE)
         .expect("Failed to add file watch");
 
-    let mut prev_video_ms = 0u64;
     let mut first_video_seg_no = 0;
+    let mut current_video_seg_no = 0;
     loop {
         let mut buffer = [0; 1024];
         let events = inotify.read_events_blocking(&mut buffer)
             .expect("Error while reading events");
-        let now_ms = (Utc::now().timestamp_millis() + ntp_epoch_offset.num_milliseconds()) as u64;
 
         for event in events {
             if let Some(file_name) = event.name {
@@ -61,46 +60,42 @@ async fn file_renamer(target: &PathBuf, filter_kind: &str) -> anyhow::Result<()>
                 let parts: Vec<&str> = file_name.split('-').collect();
                 if parts.len() == 2 && !parts[1].ends_with("continuous.mp4") {
                     let segment_no = parts[0].parse::<u32>().unwrap();
-
-                    let dst_video = target.join(Path::new(format!("{}-{}", segment_timestamp(start, segment_no), parts[1]).as_str()));
-                    let src_video = src_dir.join(Path::new(format!("{}-{}", segment_no, parts[1]).as_str()));
-                    let src_audio = src_dir.join(Path::new(format!("{}-{}", segment_no, "a0.mp4").as_str()));
-
+//TODO this is broken
                     fs::create_dir_all(target)?;
-                    //if parts[1].ends_with(filter_kind) {
-                        if parts[1].ends_with("v0.mp4") {
-                            if start_ms == 0 {
-                                start_ms = (Utc::now().timestamp_millis() + ntp_epoch_offset.num_milliseconds()) as u64;
-                                start_sec = start_ms as f64 / 1000.0;
-                                start = ((start_sec * 10.0).round() * 100.0) as u64;
-                                first_video_seg_no = segment_no;
-                                info!("first video segment: {}, time_ms: {}", segment_no, start_ms);
-                            }
-                            //info!("parts: {parts:?} dst: {dst_video:?} src: {src_video:?} src_audio: {src_audio:?}");
 
-                            if prev_video_ms != 0 {
-                                let diff = now_ms - prev_video_ms;
-                                info!("duration(ms) between segments: {} ({:03})", diff, diff as f32 /3200.0);
-                            }
-                            ffmpeg::change_timescale_ffmpeg(&src_video, &dst_video).await?;
-                            fs::remove_file(&src_video).expect("remove video failed");
-                            info!("copied video: {dst_video:?}");
-                            prev_video_ms = now_ms;
-                        } else {
-                            if start_ms == 0 || first_video_seg_no == 0 || segment_no < first_video_seg_no {
-                                info!("skipping audio segment {}, startms: {}", segment_no, start_ms);
-                                continue;
-                            }
-                            if src_audio.exists() {
-                                let dst_audio = target.join(Path::new(format!("{}-{}", segment_timestamp(start, segment_no), "a0.mp4").as_str()));
-                                fs::copy(&src_audio, &dst_audio).expect("copy audio failed");
-                                fs::remove_file(&src_audio).expect("remove audio failed");
-                                info!("copied audio: {dst_audio:?}");
-                            } else {
-                                error!("unavailable! audio: {src_audio:?}");
-                            }
+                    if parts[1].ends_with("v0.mp4") {
+                        if start_ms == 0 {
+                            start_ms = (Utc::now().timestamp_millis() + ntp_epoch_offset.num_milliseconds()) as u64;
+                            start_sec = start_ms as f64 / 1000.0;
+                            start = ((start_sec * 10.0).round() * 100.0) as u64;
+                            first_video_seg_no = segment_no;
+                            info!("first video segment: {}, time_ms: {}", segment_no, start_ms);
                         }
-                   // }
+                        current_video_seg_no = segment_no;
+
+                        let dst_video = target.join(Path::new(format!("{}-{}", segment_timestamp(start, current_video_seg_no), parts[1]).as_str()));
+                        let src_video = src_dir.join(Path::new(format!("{}-{}", current_video_seg_no, parts[1]).as_str()));
+
+                        ffmpeg::change_timescale_ffmpeg(&src_video, &dst_video).await?;
+                        fs::remove_file(&src_video).expect("remove video failed");
+                        info!("copied video: {dst_video:?}");
+                    } else {
+
+                        if segment_no == current_video_seg_no {
+                            info!("skipping audio segment {}, startms: {}", segment_no, start_ms);
+                            continue;
+                        }
+                        let src_audio = src_dir.join(Path::new(format!("{}-{}", current_video_seg_no, "a0.mp4").as_str()));
+
+                        if src_audio.exists() {
+                            let dst_audio = target.join(Path::new(format!("{}-{}", segment_timestamp(start, current_video_seg_no), "a0.mp4").as_str()));
+                            fs::copy(&src_audio, &dst_audio).expect("copy audio failed");
+                            fs::remove_file(&src_audio).expect("remove audio failed");
+                            info!("copied audio: {dst_audio:?}");
+                        } else {
+                            error!("unavailable! audio: {src_audio:?}");
+                        }
+                    }
 
                 }
             }
